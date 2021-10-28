68a1d276bce85903e1d4a7e548283d1cf8791ef1
ReznikYuriy <reznik333@gmail.com>
2021-10-28 08:04:21 +0300

    add Product Repo

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { productsProviders } from './products.providers';
 import { ProductsService } from './products.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, ...productsProviders],
 })
 export class ProductsModule {}
