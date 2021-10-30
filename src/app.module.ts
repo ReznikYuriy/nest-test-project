030153b77974450aa6448db09a443fe135b49511
ReznikYuriy <reznik333@gmail.com>
2021-10-30 22:52:46 +0300

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ProductsModule,
     DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ProductsModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
