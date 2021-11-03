444ea65d0825f9d9249c9b83424ff1a9b3deba7a
ReznikYuriy <reznik333@gmail.com>
2021-11-03 08:41:00 +0200

    add order repository to constants

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -4,3 +4,4 @@ export const TEST = 'test';
 export const PRODUCTION = 'production';
 export const PRODUCT_REPOSITORY = 'PRODUCT_REPOSITORY';
 export const USER_REPOSITORY = 'USER_REPOSITORY';
+export const ORDER_REPOSITORY = 'ORDER_REPOSITORY';
