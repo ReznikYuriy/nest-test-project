5d5e3d068636c1bb496118638145c9fb3d0bc9f8
ReznikYuriy <reznik333@gmail.com>
2021-11-03 10:27:36 +0200

    add PRODUCT_ORDER_REPOSITORY to order.provider

--- src/order/order.providers.ts
+++ src/order/order.providers.ts
@@ -1,9 +1,13 @@
-import { Order } from '../core/models';
-import { ORDER_REPOSITORY } from '../core/constants';
+import { Order, ProductOrder } from '../core/models';
+import { ORDER_REPOSITORY, PRODUCT_ORDER_REPOSITORY } from '../core/constants';
 
 export const orderProviders = [
   {
     provide: ORDER_REPOSITORY,
     useValue: Order,
+  },
+  {
+    provide: PRODUCT_ORDER_REPOSITORY,
+    useValue: ProductOrder,
   },
 ];
