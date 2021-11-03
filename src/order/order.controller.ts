635a706dd167d341fbe56c7c86425ee4a76a83a8
ReznikYuriy <reznik333@gmail.com>
2021-11-03 08:49:28 +0200

    add order contro;;er

---
+++ src/order/order.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Order } from 'src/core/models';
+import { OrderDto } from './dto';
+import { OrderService } from './order.service';
+
+@Controller('order')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  async findAll() {
+    return await this.orderService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Order> {
+    const order = await this.orderService.findOne(id);
+
+    if (!order) {
+      throw new NotFoundException("This Order doesn't exist");
+    }
+    return order;
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  async create(@Body() order: OrderDto, @Request() req): Promise<Order> {
+    return await this.orderService.create(order, req.user.id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() order: OrderDto,
+    @Request() req,
+  ): Promise<Order> {
+    const { numberOfAffectedRows, updatedOrder } =
+      await this.orderService.update(id, order, req.user.id);
+
+    if (numberOfAffectedRows === 0) {
+      throw new NotFoundException("This Order doesn't exist");
+    }
+
+    return updatedOrder;
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  async remove(@Param('id') id: string, @Request() req) {
+    const deleted = await this.orderService.delete(id, req.user.id);
+
+    if (deleted === 0) {
+      throw new NotFoundException("This Order doesn't exist");
+    }
+    return 'Successfully deleted';
+  }
+}
