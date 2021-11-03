c001f069520b052ec9987b2a6fc7db3750d8f3f3
ReznikYuriy <reznik333@gmail.com>
2021-11-03 08:40:29 +0200

    add order module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
     ProductsModule,
     UserModule,
     AuthModule,
+    OrderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
